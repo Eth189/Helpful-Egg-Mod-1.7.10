65263e55e3faf3ed5193f341b72786dc6beda5d6
Eth189 <cooldude16540@gmail.com>
2016-06-27 16:36:06 -0400

    Create CommonProxy.java

---
+++ proxies/CommonProxy.java
@@ -0,0 +1,12 @@
+package com.ethan.proxies;
+
+public class CommonProxy
+{
+  public void registerRendering() {}
+  
+  public void registerItemRenders() {}
+  
+  public void registerItemRenders2() {}
+  
+  public void registerItemRenders3() {}
+}
